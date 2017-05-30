57fc7934d24a68767e8339c698b21948870754a7
unknown <simon_verbeke_@hotmail.com>
2017-05-30 16:53:27 +0200

    Testing Server - Upload & Authentication

--- server.go
+++ server.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	http "github.com/valyala/fasthttp"
+	"time"
+	"golang.org/x/crypto/bcrypt"
+	http "./valyala/fasthttp"
 )
 
+var bcryptWorkFactor = 12
+
 func main() {
-	//http.ListenAndServe(":80", HTTPHandler)
 	h := &http.Server{
 		Handler: HTTPHandler,
 		MaxRequestBodySize: 1 * 1024 * 1024 * 1024,
@@ -15,58 +18,89 @@ func main() {
 }
 
 func HTTPHandler(ctx *http.RequestCtx) {
+	fmt.Printf("%s: %s\n", timeToString(), ctx.Path())
+
 	ctx.SetContentType("application/json")
-	fmt.Println("HttpHandler")
-	fmt.Printf("%s\n", ctx.Path())
 
 	switch string(ctx.Path()) {
 		case "/":
 			if ctx.IsGet() {
-				ctx.WriteString("Home, GET\n")
 
 			} else if ctx.IsPost() {
-				ctx.WriteString("Home, POST\n")
 
 			} else {
 				ctx.Error("{}", http.StatusNotFound)
 			}
 
+		case "/register":
+			if ctx.IsPost() {
+				username := ctx.FormValue("username");
+				password := ctx.FormValue("password");
+				var hashedPassword, _ = bcrypt.GenerateFromPassword(password, bcryptWorkFactor)
+				fmt.Println(username)
+				fmt.Println(hashedPassword)
+				//Store record in db
+				fmt.Fprintf(ctx, "{}")
+			} else {
+				ctx.Error("{}", http.StatusNotFound)
+			}
 
 		case "/video":
-			fmt.Println("case /video")
 			if ctx.IsPost() {
-				fmt.Println("is post")
-				file, error := ctx.FormFile("video")
-				if error != nil {
-					fmt.Println("ERROR ERROR")
-				}
-				fmt.Println("Going to save!")
-				fmt.Println(file)
-				http.SaveMultipartFile(file, "C:\\test\\file.mp4")
+				if authenticateRequest(ctx) {
+					fileHeader, error := ctx.FormFile("video")
 
+					if error != nil {
+						fmt.Printf("Something went wrong while trying to save the file: %s", error)
+					}
+
+					http.SaveMultipartFile(fileHeader, "C:\\test\\file.mp4")
+				} else {
+					ctx.Error("{}", http.StatusUnauthorized)
+				}
 			} else {
 				ctx.Error("{}", http.StatusNotFound)
 			}
-
 
 		case "/json":
-			fmt.Println("case /json")
 			if ctx.IsPost() {
-				fmt.Println("is post")
-				file, error := ctx.FormFile("jsonFile")
-				if error != nil {
-					fmt.Println("ERROR ERROR")
-				}
-				fmt.Println("Going to save!")
-				fmt.Println(file)
-				http.SaveMultipartFile(file, "C:\\test\\test.json")
+				if authenticateRequest(ctx) {
+					fileHeader, error := ctx.FormFile("jsonFile")
 
+					if error != nil {
+						fmt.Printf("Something went wrong while trying to save the file: %s", error)
+					}
+
+					http.SaveMultipartFile(fileHeader, "C:\\test\\test.json")
+				} else {
+					ctx.Error("{}", http.StatusUnauthorized)
+				}
 			} else {
 				ctx.Error("{}", http.StatusNotFound)
 			}
-
 
 		default:
 			ctx.Error("{}", http.StatusNotFound)
 	}
+}
+
+func authenticateRequest(ctx *http.RequestCtx) bool{
+	username := ctx.FormValue("username");
+	password := ctx.FormValue("password");
+	fmt.Println(username)
+
+	//Get record from db
+	var storedPassword []byte
+
+	var error = bcrypt.CompareHashAndPassword(storedPassword, password)
+	if error != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
+func timeToString() string {
+	now := time.Now()
+	return fmt.Sprintf("%02d:%02d:%02d.%03d", now.Hour(), now.Minute(), now.Second(), now.Nanosecond() / 1000 / 1000)
 }
