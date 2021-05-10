5ff38461926dac83d00fea3e81d34c34aee49363
Saticmotion <simon_verbeke_@hotmail.com>
2021-05-10 17:40:45 +0200

    Install vivista backup

--- VivistaServer/wwwroot/site.js
+++ VivistaServer/wwwroot/site.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 	InitDarkModeToggles();
+	InitPlayButton();
 }
 
 function UpdateDarkModeToggles() {
@@ -22,10 +23,24 @@ function InitDarkModeToggles()
 	var els = document.getElementsByClassName("dark-mode-toggle");
 	for (var i = 0; i < els.length; i++) {
 		let el = els[i];
-		el.addEventListener("change", function () {
+		el.addEventListener("change", function() {
 			data.theme = el.checked ? "dark" : "";
 			localStorage.setItem("theme", data.theme);
 			UpdateDarkModeToggles();
 		});
 	};
+}
+
+function InitPlayButton()
+{
+	var button = document.getElementsByClassName("download-button")[0];
+
+
+	button.addEventListener("click", function()
+	{
+		var message = document.getElementsByClassName("install-message")[0];
+		message.classList.remove("hidden");
+		window.location = button.dataset.uri;
+	});
+
 }
