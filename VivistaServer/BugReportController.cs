680c7ceeba71690f969031b92b4867e3f9c23888
Saticmotion <simon_verbeke_@hotmail.com>
2021-07-16 11:14:36 +0200

    Add bug report endpoint

---
+++ VivistaServer/BugReportController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Npgsql;
+using Dapper;
+
+namespace VivistaServer
+{
+	public class BugReportController
+	{
+		[Route("POST", "/api/report_bug")]
+		[Route("POST", "/api/v1/report_bug")]
+		private static async Task ReportBugPostApi(HttpContext context)
+		{
+			var form = context.Request.Form;
+			var problem = form["problem"];
+			var repro = form["repro"];
+			var email = form["email"];
+
+			if (!String.IsNullOrEmpty(problem) && !String.IsNullOrEmpty(repro))
+			{
+				using var connection = Database.OpenNewConnection();
+
+				bool success = await AddBugReport(problem, repro, email, connection);
+				await context.Response.Body.WriteAsync(Utf8Json.JsonSerializer.SerializeUnsafe(new {success}));
+			}
+			else
+			{
+				await CommonController.WriteError(context, "This request is missing data" ,StatusCodes.Status400BadRequest);
+			}
+		}
+
+		private static async Task<bool> AddBugReport(string problem, string repro, string email, NpgsqlConnection connection)
+		{
+			try
+			{
+				int success = await connection.ExecuteAsync(@"insert into bug_reports (problem, repro, email) values (@problem, @repro, @email)", new {problem, repro, email});
+				return success > 0;
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+	}
+}
