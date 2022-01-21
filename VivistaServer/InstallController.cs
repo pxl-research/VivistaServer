6f6c50067f8b3f4ef8be09f8bec0478c4814c30a
Saticmotion <simon_verbeke_@hotmail.com>
2022-01-21 15:59:11 +0100

    Endpoints for the Auto Updater

--- VivistaServer/InstallController.cs
+++ VivistaServer/InstallController.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Fluid;
@@ -10,6 +11,37 @@ namespace VivistaServer
 	{
 		private static string playerFolder = Path.Combine(CommonController.wwwroot, "Installers", "Player");
 		private static string editorFolder = Path.Combine(CommonController.wwwroot, "Installers", "Editor");
+
+		private static string cachedVersionNumber;
+		private static DateTime cacheTime;
+		private static TimeSpan maxCacheTime = new TimeSpan(0, 10, 0);
+
+		//NOTE(Simon): The number is not actually that important. Client-side we just want to check if the number is the same or not. If not, suggest update to user.
+		[Route("GET", "/api/latest_version_number")]
+		[Route("GET", "/api/v1/latest_version_number")]
+		private static async Task LatestVersionNumberGet(HttpContext context)
+		{
+			string version;
+			//NOTE(Simon): Cache time still valid
+			if (cacheTime > DateTime.UtcNow - maxCacheTime)
+			{
+				version = cachedVersionNumber;
+			}
+			else
+			{
+				var installerName = new DirectoryInfo(playerFolder)
+					.GetFiles("*.exe")
+					.OrderBy(x => x.LastWriteTime)
+					.First().Name;
+				//NOTE(Simon): String looks like "VivistaPlayer-x.x.x.exe. So we take the substring from the first dash to the last period.
+				var lastPart = installerName.Substring(installerName.IndexOf('-') + 1);
+				version = lastPart.Substring(0, lastPart.LastIndexOf('.'));
+				cachedVersionNumber = version;
+				cacheTime = DateTime.UtcNow;
+			}
+
+			await context.Response.WriteAsJsonAsync(new {version});
+		}
 
 		[Route("GET", "/install")]
 		private static async Task InstallGet(HttpContext context)
@@ -31,6 +63,14 @@ namespace VivistaServer
 			context.Response.Redirect(GetLatestPlayerInstallerURL());
 		}
 
+		[Route("GET", "/api/latest_version_player_url")]
+		[Route("GET", "/api/v1/latest_version_player_url")]
+		private static async Task LatestPlayerURLGet(HttpContext context)
+		{
+			var url = GetLatestPlayerInstallerURL();
+			await context.Response.WriteAsJsonAsync(url);
+		}
+
 		private static string GetLatestPlayerInstallerURL()
 		{
 			var latestPlayerInstaller = new DirectoryInfo(playerFolder)
@@ -38,6 +78,14 @@ namespace VivistaServer
 									.OrderBy(x => x.LastWriteTime)
 									.First();
 			return GetURLFromAbsolutePath(latestPlayerInstaller.FullName);
+		}
+
+		[Route("GET", "/api/latest_version_editor_url")]
+		[Route("GET", "/api/v1/latest_version_editor_url")]
+		private static async Task LatestEditorURLGet(HttpContext context)
+		{
+			var url = GetLatestEditorInstallerURL();
+			await context.Response.WriteAsJsonAsync(url);
 		}
 
 		private static string GetLatestEditorInstallerURL()
