102ab19f01fb26bd6f3223159b1bb7a41c4bea78
Tom Vanbriel <tom.vanbriel@student.pxl.be>
2022-03-30 13:29:14 +0200

    - role controller - role.liquid

---
+++ VivistaServer/RoleController.cs
@@ -0,0 +1,201 @@
+﻿using Fluid;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using static VivistaServer.CommonController;
+using System.Collections.Generic;
+using System;
+using Dapper;
+using Npgsql;
+
+namespace VivistaServer
+{
+	public class RoleController
+	{
+		private static List<Role> allRoles;
+
+		[Route("GET", "/admin/role")]
+		private static async Task OpenRolesPage(HttpContext context)
+		{
+			if (await IsUserAdmin(context))
+			{
+				await LoadRolesPage(context);
+			}
+			else
+			{
+				CommonController.Write404(context);
+			}
+		}
+
+		[Route("POST", "/admin/role")]
+		private static async Task AddAdmin(HttpContext context)
+		{
+			if (await IsUserAdmin(context))
+			{
+				int roleid = GetRoleId("Admin");
+
+				var form = context.Request.Form;
+				var username = form["username"].ToString();
+
+				using var connection = Database.OpenNewConnection();
+				int userid = 0;
+				userid = await Database.QuerySingleOrDefaultAsync<int>(connection, "SELECT userId FROM USERS WHERE username = @username;", context, new { username });
+				if (userid > 0)
+				{
+					if (!await UserRoleExist(userid, roleid, connection, context))
+					{
+						await Database.ExecuteAsync(connection, "INSERT INTO user_roles(userid, roleid) VALUES(@userid, @roleid)", context, new { userid, roleid });
+						await LoadRolesPage(context, "", $"{username} is a new admin!");
+					}
+					else
+					{
+						await LoadRolesPage(context, $"{username} is already admin.");
+					}
+				}
+				else
+				{
+					await LoadRolesPage(context, "Username doesn't exist");
+				}
+			}
+			else
+			{
+				CommonController.Write404(context);
+			}
+		}
+
+		[Route("GET", "/admin/deleteAdmin")]
+		private static async Task DeleteAdmin(HttpContext context)
+		{
+			if (await IsUserAdmin(context))
+			{
+				var username = context.Request.Query["username"].ToString();
+
+				using var connection = Database.OpenNewConnection();
+				var userid = await Database.QuerySingleOrDefaultAsync<int>(connection, "SELECT userid FROM users WHERE username = @username;", context, new { username });
+
+				var roleid = GetRoleId("Admin");
+				var loggedInUser = await UserSessions.GetLoggedInUser(context);
+				if (userid > 0)
+				{
+					if (loggedInUser.userid != userid)
+					{
+						try
+						{
+							await Database.ExecuteAsync(connection, "DELETE FROM user_roles WHERE userid = @userid AND roleid = @roleid;", context, new { userid, roleid });
+							await LoadRolesPage(context, "", $"{username} his admin role is deleted.");
+						}
+						catch (Exception ex)
+						{
+							await LoadRolesPage(context, "Something went wrong");
+						}
+					}
+					else
+					{
+						await LoadRolesPage(context, "You can't delete your own admin role.");
+					}
+				}
+				else
+				{
+					await LoadRolesPage(context, "User doesn't exist");
+				}
+			}
+			else
+			{
+				CommonController.Write404(context);
+			}
+		}
+
+		private static async Task LoadRolesPage(HttpContext context, string error = "", string success = "")
+		{
+			var roleid = GetRoleId("Admin");
+
+			SetHTMLContentType(context);
+			var adminTask = Task.Run(async () =>
+			{
+				await using var connection = Database.OpenNewConnection();
+				return await Database.QueryAsync<User>(connection,
+					"SELECT users.username as username FROM users INNER JOIN user_roles ON users.userid = user_roles.userid WHERE roleid = @roleid;", context, new { roleid });
+			});
+
+			//var admins = await adminTask;
+			adminTask.Wait();
+
+
+			var templateContext = new TemplateContext(new
+			{
+				admins = adminTask.Result.Select(a => new { a.username }).ToList(),
+				//admins = admins.Select(a => new { a.username }).ToList(),
+				error = error,
+				success = success
+			});
+
+			await context.Response.WriteAsync(await HTMLRenderer.Render(context, "Templates\\roles.liquid", templateContext));
+		}
+
+		public static int GetRoleId(string role)
+		{
+			int roleid = 0;
+			try
+			{
+				foreach (var r in allRoles)
+				{
+					if (r.name == role)
+					{
+						roleid = (int)r.id;
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				CommonController.LogDebug(ex.StackTrace);
+			}
+			Debug.Assert((roleid > 0), $"{role} doesn't exist");
+			return roleid;
+		}
+
+		public static async Task LoadRoles()
+		{
+			using var connection = Database.OpenNewConnection();
+			var roles = await connection.QueryAsync<Role>(@"SELECT id, name FROM roles;");
+			allRoles = (List<Role>)roles;
+
+#if DEBUG
+			CommonController.LogDebug("All roles:");
+			foreach (var role in allRoles)
+			{
+				CommonController.LogDebug($"id: {role.id} - name: {role.name}");
+			}
+#endif
+		}
+
+		public async static Task<bool> UserRoleExist(int userid, int roleid, NpgsqlConnection connection, HttpContext context)
+		{
+			try
+			{
+				int count = await Database.QuerySingleAsync<int>(connection, "select count(*) from user_roles where userid = @userid AND roleid = @roleid", context, new { userid, roleid });
+				return count > 0;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public async static Task<bool> IsUserAdmin(HttpContext context)
+		{
+			var user = await UserSessions.GetLoggedInUser(context);
+			if (user != null)
+			{
+				using var connection = Database.OpenNewConnection();
+				var roles = await user.GetRoles(connection, context);
+				var adminId = RoleController.GetRoleId("Admin");
+				if (roles != null && roles.Contains(adminId))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
